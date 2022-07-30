a7ef4807f739795b2b3179441bc8615e013e2e41
Vishnu C Prasad <mail@vishnucprasad.in>
2022-07-30 20:43:05 +0530

    fixed statusbar color issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:expensify/core/colors.dart';
-import 'package:expensify/presentation/main_page/main_page_screen.dart';
 import 'package:expensify/presentation/splash/splash_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp());
@@ -12,10 +11,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: primaryColor, //or set color with: Color(0xFF0000FF)
+      ),
+    );
     return MaterialApp(
       theme: ThemeData(
         primaryColor: primaryColor,
-        backgroundColor: backgroundColor,
+        backgroundColor: primaryColor,
         scaffoldBackgroundColor: backgroundColor,
         fontFamily: GoogleFonts.montserrat().fontFamily,
         textTheme: const TextTheme(
